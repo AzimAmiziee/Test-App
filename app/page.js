45efa530c2b34e241ee6b346871e557055803071
Azim <itsayeazim@gmail.com>
2026-03-18 23:35:00 +0800

    test

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Hi, welcome to Azim's website!</h1>
-        <p>We're glad to have you here. Explore and enjoy your stay!</p>
+        <p>Testting 123!</p>
       </main>
     </div>
   );
